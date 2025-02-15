4776ee4d9dcac769852e3ec6d4d6214c55f71911
Naresh Kumar Vaid <nkraj021@gmail.com>
2025-02-15 10:36:39 +0530

    connect supbase

---
+++ lib/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SupabaseHome extends StatelessWidget {
+  const SupabaseHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("dfdf"),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_first/homepage.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "",
+    anonKey: "",
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supabase Connect',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SupabaseHome(),
+    );
+  }
+}
